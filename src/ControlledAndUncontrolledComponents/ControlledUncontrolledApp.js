2560e5e9524d5e449f61725ee02c54bd60bfbe39
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-11-01 16:34:16 +0200

    Controlled Modal

--- src/ControlledAndUncontrolledComponents/ControlledUncontrolledApp.js
+++ src/ControlledAndUncontrolledComponents/ControlledUncontrolledApp.js
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import { ControlledForm } from "./ControlledForm";
+import { ControlledModal } from "./ControlledModal";
+import { UncontrolledModal } from "./UncontrolledModal";
 import { UncontrolledForm } from "./UncontrolledForm";
 
 function ControlledUncontrolledApp() {
-  return <ControlledForm />;
+  const [shouldShowModal, setShouldShowModal] = useState(false);
+  return (
+    <>
+      <ControlledModal
+        shouldShow={shouldShowModal}
+        onRequestClose={() => setShouldShowModal(false)}
+      >
+        <h1>Hello</h1>
+      </ControlledModal>
+
+      <button onClick={() => setShouldShowModal(!shouldShowModal)}>
+        {shouldShowModal ? "Close Modal" : "Open Modal"}
+      </button>
+
+      <ControlledForm />
+    </>
+  );
 }
 
 export default ControlledUncontrolledApp;
