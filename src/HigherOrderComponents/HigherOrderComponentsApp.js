391b5f3a7fa35697c38bc3a9b1bdb628b37e1be9
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-11-01 20:13:59 +0200

    HOC improvements

--- src/HigherOrderComponents/HigherOrderComponentsApp.js
+++ src/HigherOrderComponents/HigherOrderComponentsApp.js
@@ -1,15 +1,17 @@
 import { printProps } from "./printProps";
 import { UserInfo } from "./UserInfo";
+import { UserInfoForm } from "./UserInfoForm";
 import { withUser } from "./withUser";
 
 const UserInfoWrapped = printProps(UserInfo);
-const UserInfoWithLoader = withUser(UserInfo, "222");
+const UserInfoWithLoader = withUser(UserInfo, "123");
 
 function App() {
   return (
     <>
       <UserInfoWrapped a={1} b="Hello" c={{ name: "Denis" }} />
       <UserInfoWithLoader />
+      <UserInfoForm />
     </>
   );
 }
