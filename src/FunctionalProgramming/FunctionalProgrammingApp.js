0a1f976a40c7658b56cc296626b498361a017f28
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-11-02 10:54:39 +0200

    Recursive components

---
+++ src/FunctionalProgramming/FunctionalProgrammingApp.js
@@ -0,0 +1,29 @@
+import { RecursiveComponent } from "./RecursiveComponent";
+
+const nestedObject = {
+  a: 1,
+  b: {
+    b1: 4,
+    b2: {
+      b23: "Hello",
+    },
+    b3: {
+      b31: {
+        message: "Hi",
+      },
+      b32: {
+        message: "Hi",
+      },
+    },
+  },
+  c: {
+    c1: 2,
+    c2: 3,
+  },
+};
+
+function FunctionalProgrammingApp() {
+  return <RecursiveComponent data={nestedObject} />;
+}
+
+export default FunctionalProgrammingApp;
